a58bb4f01db702f17869548aea75b8e064d98090
anurag262000 <anuragmishra262000@gmail.com>
2024-07-19 15:40:39 +0530

    origen changed

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || 'https://goldstarsewing.vercel.app/',
   credentials: true,
 }));
 
